1e98a5467a83ddbe2ffd679150c78225cdacfb0c
edersilva <seder0802@gmail.com>
2025-11-26 17:14:15 -0300

    chore: Force cache invalidation with build comment

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Build: 2025-01-26-v3 - Force cache invalidation
 createRoot(document.getElementById("root")!).render(<App />);
